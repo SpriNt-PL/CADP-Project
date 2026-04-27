0e9b50664becee2958e570a73f8c3094af5bd2f3
Kuba <kuba@example.com>
2026-04-27 10:18:14 +0200

    Player and Enemy are smaller now

--- Enemy.py
+++ Enemy.py
@@ -6,6 +6,12 @@ from entity import Entity
 class Enemy(Entity):
     def __init__(self, x, y, player):
         super().__init__(x, y, 'assets/enemy.png')
+
+        # Resizing 
+        new_size = (self.image.get_width() // 2, self.image.get_height() // 2)
+        self.image = pygame.transform.smoothscale(self.image, new_size)
+        self.rect = self.image.get_rect(center=self.pos)
+
         self.player = player
         self.velocity = 200
         self.rotation = 0
--- player.py
+++ player.py
@@ -4,6 +4,12 @@ from entity import Entity
 class Player(Entity):
     def __init__(self, x, y):
         super().__init__(x, y, 'assets/Player.png')
+
+         # Resizing 
+        new_size = (self.image.get_width() // 2, self.image.get_height() // 2)
+        self.image = pygame.transform.smoothscale(self.image, new_size)
+        self.rect = self.image.get_rect(center=self.pos)
+
         self.velocity = 600
         self.direction = pygame.Vector2(0, 0)
 
